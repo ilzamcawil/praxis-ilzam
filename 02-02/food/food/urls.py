cf4f426f5331313e7ee4d7fd8d0e010733a2255a
ilzamcawil <ilzam.inf18@student.unu-jogja.ac.id>
2021-10-13 16:55:53 +0700

    mengedit table pesanan 02-03

--- 02-02/food/food/urls.py
+++ 02-02/food/food/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('makanan.urls')),
     path('minuman/', include('minuman.urls')),
+    path('pesanan/', include('pesanan.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ 02-02/food/pesanan/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.readPesanan, name='readPesanan'),
+    path('minuman/', views.readPesanan, name='readPesanan'),
+    path('hapus/<id>',views.hapus),
+    path('edit/<id>/', views.edit),
+    path('detail/<id>/', views.detail),
+
+]
